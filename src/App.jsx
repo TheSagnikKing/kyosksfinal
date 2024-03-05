bd7eab3afd88ab66c34b0078d85ffd87dd94e74d
TheSagnikKing <sagniknandy27@gmail.com>
2024-03-05 19:27:46 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const Public = React.lazy(() => import("./components/public/Public"));
 const JoinQueue = React.lazy(() => import("./components/JoinQueue/JoinQueue"));
 const BarberSignin = React.lazy(() => import("./components/barber/Signin/Signin"));
+const Table = React.lazy(() => import("./components/barber/demo/Table"))
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Public />} />
           <Route path="/joinqueue" element={<JoinQueue />} />
           <Route path="/barbersignin" element={<BarberSignin />} />
+          <Route path='/table' element={<Table/>}/>
         </Routes>
       </Suspense>
     </BrowserRouter>
