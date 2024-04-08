dbd7ea423bd93c200b80715a3caf867af2bd6af8
TheSagnikKing <sagniknandy27@gmail.com>
2024-04-08 19:11:16 +0530

    commit

--- src/components/QueueList/QueueList.jsx
+++ src/components/QueueList/QueueList.jsx
@@ -5,6 +5,8 @@ import { selectCurrentAdminInfo } from '../AdminSignin/adminauthSlice'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { IoMdHome } from 'react-icons/io'
+import { PiQueueBold } from 'react-icons/pi'
+import { GiCancel } from 'react-icons/gi'
 
 const QueueList = () => {
 
@@ -18,13 +20,26 @@ const QueueList = () => {
     isLoading
   } = useGetQlistBySalonIdKioskQuery(adminInfo?.salonId)
 
+
+  console.log(data)
+
+  const serverHandler = (barberId,service,_id) => {
+    console.log(barberId)
+    console.log(service)
+    console.log(_id)
+  }
+
+  const cancelHandler = () => {
+
+  }
+
   // Check if data is available and it is not loading or erroring
   if (isSuccess && !isLoading && !isError && data) {
     return (
       <div className='queuelist__container__main__wrapper'>
         <Link to="/kiyosk"
-            className='homeiconClassQueue'
-          ><IoMdHome /></Link>
+          className='homeiconClassQueue'
+        ><IoMdHome /></Link>
         <div className='queuelist__container__main'>
           <h1>Queue List</h1>
           {/* <pre style={{fontSize:"1.7rem",fontWeight:"500"}}>{JSON.stringify(data, null, 2)}</pre> */}
@@ -36,6 +51,8 @@ const QueueList = () => {
               <td>Services</td>
               <td>Method Used</td>
               <td>Q Position</td>
+              <td>Served</td>
+              <td>Cencel</td>
             </th>
 
             {
@@ -47,6 +64,12 @@ const QueueList = () => {
                   <td>{q.services.map((s) => <span style={{ marginRight: "0.5rem" }}>{s.serviceName}</span>)}</td>
                   <td>{q.methodUsed}</td>
                   <td>{q.qPosition}</td>
+                  <td className='que-serve' onClick={() => serverHandler(q.barberId, q.services, q._id)}>
+                    <PiQueueBold />
+                  </td>
+                  <td className='que-cancel' onClick={() => cancelHandler()}>
+                    <GiCancel />
+                  </td>
                 </tr>
               ))
             }
@@ -67,11 +90,11 @@ const QueueList = () => {
     return (
       <div className='queuelist__container__main__wrapper'>
         <Link to="/kiyosk"
-            className='homeiconClassQueue'
-          ><IoMdHome /></Link>
+          className='homeiconClassQueue'
+        ><IoMdHome /></Link>
         <div className='queuelist__container__main'>
-          <h1 
-          style={{color:"red",fontWeight:"bold",fontSize:"3rem"}}
+          <h1
+            style={{ color: "red", fontWeight: "bold", fontSize: "3rem" }}
           >No QueueList Available</h1>
         </div>
       </div>
