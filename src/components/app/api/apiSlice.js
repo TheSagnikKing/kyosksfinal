346f74a1e1aecbc920f304ea3b7261b8837c1bf7
Sumit <meathdev@outlook.com>
2024-11-21 18:39:07 +0530

    base url changed

--- src/components/app/api/apiSlice.js
+++ src/components/app/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: "api",
     credentials: 'include',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://iqb-kiosk.onrender.com' }),
+    // baseQuery: fetchBaseQuery({ baseUrl: 'https://iqb-kiosk.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://iqb-final.onrender.com' }),
     tagTypes: ['adminloggin'],
     endpoints: builder => ({})
 })
